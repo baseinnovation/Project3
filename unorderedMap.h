7922d891cf6ce9a5cb96e0069b47417e0859f245
cathyquan <66438377+cathyquan@users.noreply.github.com>
2022-07-31 14:00:39 -0400

    Add files via upload

--- unorderedMap.h
+++ unorderedMap.h
@@ -1,35 +1,57 @@
-#include "Bin.h"
+#include <string>
+#include <iostream>
 using namespace std;
 
 template <typename T>
 class Unordered_Map {
 private:
-    list<list<pair<int, T>>> *buckets;
+    vector<vector<pair<string, T>>> buckets;
     int bucketSize;
     int numElements;
     float maxLoadFactor;
     hash<string> stringHash;
 
 public:
+    Unordered_Map(int bucketSize) {
+        this->bucketSize = 10;
+        this->numElements = 0;
+        this->maxLoadFactor = 0.8f;
+    }
+
     int hash(string key) {
         int hashedKey = stringHash(key);
         return hashedKey % bucketSize;
     }
 
-    list<int>::iterator find(string key) {
-        for (auto i: buckets[hash(key)]) {
-            for (auto j: i)
-                if (j.first == key)
-                    return j;
+    void rehash() {
+        if (numElements / bucketSize <= maxLoadFactor)
+            return;
+        bucketSize *= 2;
+        vector<vector<pair<string, T>>> newBuckets(bucketSize);
+        for (int i = 0; i < buckets.size(); i++) {
+            for (int j = 0; j < buckets[i]; j++) {
+                int newHash = hash(buckets[i][j].first);
+                newBuckets[newHash].push_back({buckets[i][j].first, buckets[i][j].second});
+            }
+            buckets = newBuckets;
         }
-        return buckets[hash(key)].end();
+    }
+
+    T find(string key) {
+        for (int i = 0; i < buckets[hash(key)].size(); i++) {
+            if (buckets[hash(key)][i].first == key)
+                return buckets[hash(key)][i].second;
+        }
+        cout << "does not exist" << endl;
+        return nullptr;
     }
 
     void insert(string key, T value) {
-        auto iter = find(key);
-        if (iter != buckets[hash(key)].end()) {
-            iter->second = value;
-            return;
+        for (int i = 0; i < buckets[hash(key)].size(); i++) {
+            if (buckets[hash(key)][i].first == key) {
+                buckets[hash(key)][i].second = value;
+                return;
+            }
         }
         buckets[hash(key)].push_back({key, value});
         numElements++;
