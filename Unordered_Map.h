bcde13de76adcd39b28c5304e13e4f60f654706a
cathyquan <66438377+cathyquan@users.noreply.github.com>
2022-07-31 22:41:34 -0400

    Add files via upload

---
+++ Unordered_Map.h
@@ -0,0 +1,70 @@
+//
+// Created by Cathy Quan on 7/31/22.
+//
+
+#ifndef UNTITLED_UNORDERED_MAP_H
+#define UNTITLED_UNORDERED_MAP_H
+
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <typename T>
+class Unordered_Map {
+private:
+    vector<vector<pair<string, vector<T>>>> buckets;
+    int bucketSize;
+    int numElements;
+    float maxLoadFactor;
+    hash<string> stringHash;
+
+public:
+    Unordered_Map() {
+        bucketSize = 10;
+        numElements = 0;
+        maxLoadFactor = 0.8f;
+    }
+
+    int hash(string key) {
+        int hashedKey = stringHash(key);
+        return hashedKey % bucketSize;
+    }
+
+    void rehash() {
+        if (numElements / bucketSize <= maxLoadFactor)
+            return;
+        bucketSize *= 2;
+        vector<vector<pair<string, vector<T>>>> newBuckets(bucketSize);
+        for (int i = 0; i < buckets.size(); i++) {
+            for (int j = 0; j < buckets[i]; j++) {
+                int newHash = hash(buckets[i][j].first);
+                newBuckets[newHash].push_back({buckets[i][j].first, buckets[i][j].second});
+            }
+            buckets = newBuckets;
+        }
+    }
+
+    T find(string key) {
+        for (int i = 0; i < buckets[hash(key)].size(); i++) {
+            if (buckets[hash(key)][i].first == key)
+                return buckets[hash(key)][i].second;
+        }
+        cout << "does not exist" << endl;
+        return nullptr;
+    }
+
+    void insert(string key, T value) {
+        for (int i = 0; i < buckets[hash(key)].size(); i++) {
+            if (buckets[hash(key)][i].first == key) {
+                buckets[hash(key)][i].second = static_cast<initializer_list<Recipe>>(static_cast<const vector<struct Recipe>>(value));
+                return;
+            }
+        }
+        buckets[hash(key)].push_back({key, value});
+        numElements++;
+        rehash();
+    }
+};
+
+#endif //UNTITLED_UNORDERED_MAP_H
